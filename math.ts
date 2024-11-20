227d31f6e999373d6b398841a1da3e1a1ca7661e
Noah <noahlezama2000@gmail.com>
2024-11-20 08:47:39 +0100

    Aufgabe 2c

---
+++ math.ts
@@ -0,0 +1,19 @@
+const sum = (a: number, b: number) => a + b;
+
+const sub = (a: number, b: number) => a - b;
+
+const mult = (a: number, b: number) => a * b;
+
+const div = (a: number, b: number) => a / b;
+
+console.log("SUM string:", sum("1", "2"));
+console.log("SUM numeber:", sum(1, 2));
+
+console.log("SUB string:", sub("1", "2"));
+console.log("SUB numeber:", sub(1, 2));
+
+console.log("MULT string:", mult("1", "2"));
+console.log("MULT numeber:", mult(1, 2));
+
+console.log("DIV string:", div("1", "2"));
+console.log("DIV numeber:", div(1, 2));

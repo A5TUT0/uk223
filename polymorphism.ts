bcc44ae445ff1f540f813b41f26113c4d7923f02
Noah <noahlezama2000@gmail.com>
2024-11-20 16:56:54 +0100

    Aufabe 3

---
+++ polymorphism.ts
@@ -0,0 +1,60 @@
+class Fahrzeug {
+  private marke: string;
+  private baujahr: number;
+  private tankfull: number;
+
+  constructor(marke: string, baujahr: number, tankfull: number) {
+    this.marke = marke;
+    this.baujahr = baujahr;
+    this.tankfull = tankfull;
+  }
+  fahren() {
+    return "Das Fahrzeug fährt und ist von der Marke " + this.marke;
+  }
+}
+const Lambo = new Fahrzeug("Lamborgini", 2030, 1000000);
+console.log(Lambo.fahren());
+
+class Auto extends Fahrzeug {
+  private turen: number;
+  constructor(turen: number, marke: string, baujahr: number, tankfull: number) {
+    super(marke, baujahr, tankfull);
+    this.turen = turen;
+  }
+  numberOfTuren() {
+    return this.turen;
+  }
+  hupen() {
+    return "PIIIIIIIIIIIIIIIIII PIIIIIIIIIII PIIIIIIIIIII";
+  }
+}
+const Ferrari = new Auto(2, "Ferrari", 1910, 10);
+console.log(Ferrari.fahren());
+console.log(Ferrari.hupen());
+console.log(Ferrari.numberOfTuren());
+// c)  Die Eigenschaft muss im Konstruktor mit allen anderen Eigenschaften der Basisklasse mitgegeben werden. Weiter soll die Klasse eine Methode «Beladen» besitzen welche es erlaubt das Fahrzeug zu beladen. Die Maximale Zuglast darf aber nicht überschritten werden.
+
+class Lastwagen extends Fahrzeug {
+  private maxWight: number;
+
+  constructor(
+    marke: string,
+    baujahr: number,
+    tankfull: number,
+    maxWight: number
+  ) {
+    super(marke, baujahr, tankfull);
+    this.maxWight = maxWight;
+  }
+  wight() {
+    return this.maxWight;
+  }
+  beladen(index: number) {
+    if (index > this.maxWight) return "😅 Your auto is to heavy";
+    return "👍 You can pass";
+  }
+}
+const Ferrari2 = new Auto(2, "Ferrari", 1910, 10);
+const Lastwagen1 = new Lastwagen("SBB", 2000, 1000, 10000);
+
+console.log(Lastwagen1.beladen(10));

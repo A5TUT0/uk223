ec0f71323ee9324a2b1832dbe995b6a41a5bcb30
Noah <noahlezama2000@gmail.com>
2024-11-20 09:17:41 +0100

    Aufgabe 2d

--- math.ts
+++ math.ts
@@ -6,14 +6,10 @@ const mult = (a: number, b: number) => a * b;
 
 const div = (a: number, b: number) => a / b;
 
-console.log("SUM string:", sum("1", "2"));
 console.log("SUM numeber:", sum(1, 2));
 
-console.log("SUB string:", sub("1", "2"));
 console.log("SUB numeber:", sub(1, 2));
 
-console.log("MULT string:", mult("1", "2"));
 console.log("MULT numeber:", mult(1, 2));
 
-console.log("DIV string:", div("1", "2"));
 console.log("DIV numeber:", div(1, 2));
